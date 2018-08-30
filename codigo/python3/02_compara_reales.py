504b87db9fe6aa936c0521a95a51cc75676f77c7
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2018-08-30 14:16:47 -0500

    Se puso el encabezado al archivo

--- codigo/python3/02_compara_reales.py
+++ codigo/python3/02_compara_reales.py
@@ -1,10 +1,12 @@
+# Este programa enseña que no debemos comparar flotantes con ==
+
 import math
 
 # (x - sqrt(5))^2 = x^2 - 2*sqrt(5) x + 5
 a = 1
 b = -2*math.sqrt(5)       # a*x^2 + b*x + c = 0
 c = 5;                    # x^2 - 2*sqrt(5) + 5 = 0
-   
+
 d2 = b**2 - 4.0*a*c;  print("d2 = ", d2) # debe dar 0
 d  = math.sqrt(d2);   print("d  = ", d)  # debe dar 0
 x1 = (-b + d)/(2*a);  print("x1 = ", x1)
