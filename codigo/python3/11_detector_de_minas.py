1c88ce01f79981cfbb486abe2e21d42cc9929206
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2018-09-27 14:27:50 -0500

    Eliminando código innecesario

--- codigo/python3/11_detector_de_minas.py
+++ codigo/python3/11_detector_de_minas.py
@@ -3,7 +3,7 @@
 #   Tablero de juego:
 #
 #       | 0 1 2 3 4 5 6 7 8 9 |        intento   mina1   mina2   mina3
-#    ---x---------------------x           1
+#    ---+---------------------+           1
 #     A |                     |           2
 #     B |                     |           3
 #     C |                     |           4
@@ -14,25 +14,25 @@
 #     H |                     |           9
 #     I |                     |          10
 #     J |                     |
-#    ---x---------------------x
+#    ---+---------------------+
 
 import random
 
-def leer_radar(X, Y, m):
-   if m['x'] == X and m['y'] == Y: m['detectada'] = True
+def leer_radar(C, F, m):
+   if m['c'] == C and m['f'] == F: m['detectada'] = True
 
    if m['detectada']:
       print('DETECTADA')
       return
 
-   if m['y'] > Y:
+   if m['f'] > F:
       print('S', end='')
-   elif m['y'] < Y:
+   elif m['f'] < F:
       print('N', end='')
 
-   if m['x'] > X:
+   if m['c'] > C:
       print('E', end='')
-   elif m['x'] < X:
+   elif m['c'] < C:
       print('W', end='')
 
    print('')
@@ -43,37 +43,33 @@ N_MINAS =  3 # número de minas
 IMAX    = 10 # número de iteraciones máximo
 
 # Se define el vector (diccionario) que contendrá las minas
-m = [{'x': None, 'y': None, 'detectada': False} for i in range(N_MINAS)]  
+m = [{'c': None, 'f': None, 'detectada': False} for i in range(N_MINAS)]  
 
-# Generar el número secreto
-s = [random.randint(0, 9) for i in range(N_MINAS)]
-
-# Inicializar la posición de las minas
+# Inicializar la posición de las minas aleatoriamente
 for i in range(N_MINAS):
-    m[i]['x']         = random.randint(0, 9)
-    m[i]['y']         = random.randint(0, 9)
-    m[i]['detectada'] = False
+    m[i]['c'] = random.randint(0, 9)
+    m[i]['f'] = random.randint(0, 9)
 
 for i in range(IMAX):
     # Leer el fila y columna desde el teclado
     while True:
         num = input("Entre las coordenadas de la mina A..J, 0..9 = ")
-        Y = ord(num[0].upper()) - ord('A') # Fila: A=0, B=1, C=2, ... J=9
-        X = int(num[1])                   # Columna
-        if len(num) == 2 and 0 <= X <= 9 and 0 <= Y <= 9: break
+        F = ord(num[0].upper()) - ord('A') # Fila: A=0, B=1, C=2, ... J=9
+        C = int(num[1])                    # Columna
+        if len(num)==2 and 0<=C<=9 and 0<=F<=9: break
     
     num_minas_detectadas = 0 # número de minas detectadas
     for j in range(N_MINAS):
-        print('Mina {0}: '.format(j+1), end = '')
-        leer_radar(X, Y, m[j])
+        print(f'Mina {j+1}: ', end = '')
+        leer_radar(C, F, m[j])
         if m[j]['detectada']: num_minas_detectadas += 1
   
     if num_minas_detectadas == N_MINAS:
-        print('\nGANASTE!!! Utilizaste {0} intentos'.format(i))
+        print(f'\nGANASTE!!! Utilizaste {i} intentos')
         break
 else:
     print('\nPERDISTE :-( Las minas estaban localizadas en:')
     for j in range(N_MINAS):
-        print('Mina {0}: {1}{2}'.format(j+1, chr(m[j]['y']+ord('A')), m[j]['x']))
+        print('Mina {0}: {1}{2}'.format(j+1, chr(m[j]['f']+ord('A')), m[j]['c']))
 
     print('Inténtalo de nuevo')
