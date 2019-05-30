4455f4466b4a727fcad42286c07e0a21aa9ac5c4
Diego Andrés Alvarez Marín <daalvarez@unal.edu.co>
2019-05-30 09:03:15 -0500

    time.perf_counter() y cambio de variable en la actualización

--- codigo/python3/04_mas_vs_append.py
+++ codigo/python3/04_mas_vs_append.py
@@ -1,13 +1,13 @@
 import time
 
-a = list(range(10000000))
-tic = time.time()
-b = a + ['xxx']
-toc = time.time()
+a = list(range(10_000_000))
+tic = time.perf_counter()
+a = a + ['xxx']
+toc = time.perf_counter()
 print('El proceso duró', toc-tic, 'segundos')
 
-tic = time.time()
+tic = time.perf_counter()
 a.append('xxx')  # Observe que append es más eficiente que +
-toc = time.time()
+toc = time.perf_counter()
 print('El proceso duró', toc-tic, 'segundos')
 
