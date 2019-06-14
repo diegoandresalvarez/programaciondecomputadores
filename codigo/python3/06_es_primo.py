3d06ebcd44ada942f1586725b0a086faf174375b
Diego Andrés Alvarez Marín <diegotorquemada@gmail.com>
2019-06-14 12:06:25 -0500

    Mejorando el código

--- codigo/python3/06_es_primo.py
+++ codigo/python3/06_es_primo.py
@@ -1,34 +1,32 @@
 def es_primo(num):
-    '''
-    es_primo(num)
+    '''Detemina si un número es primo o identifica los primos de una lista/tupla
 
-    Función que detemina si num es primo o no
+    es_primo(num)
     
     Parametro de entrada:
-    num: número entero o lista de números enteros
+    num: número entero, tupla o lista
     
     Retorna:
     Si num es un entero, retorna True o False dependiendo si num es primo
-    Si num es una lista de enteros, retorna un vector con True o False 
-    dependiendo si num los elementos de num son primos o no.         
+    Si num es una lista o tupla de enteros, retorna un vector con True o False 
+    dependiendo si los elementos de num son primos o no.
     '''
-    try:
-        if isinstance(num, int):       # en caso que num sea un entero
-            if num == 2:
-                return True
-            elif (num < 2) or(num%2 == 0):
-                return False
-            else:
-                for i in range(3, num//2 + 1, 2):
-                    if num%i == 0:
-                        return False
-                return True
-        elif isinstance(num, list):    # en caso que num sea una lista
-            vec_es_primo = [es_primo(num[i]) for i in range(len(num))]
-            return vec_es_primo
-    except:
+    if isinstance(num, int):       # en caso que num sea un entero
+        if num == 2:
+            return True
+        elif (num < 2) or(num%2 == 0):
+            return False
+        else:
+            for i in range(3, num//2 + 1, 2):
+                if num%i == 0:
+                    return False
+            return True
+    elif type(num) in (tuple, list):  # en caso que num sea una tupla o lista
+        vec_es_primo = [es_primo(numero) for numero in num]
+        return vec_es_primo
+    else:
         return False
 
 print(es_primo(37))
-print(es_primo([97,98,99,100,101,102,103]))
+print(es_primo([97,98,99,'Hola',101,102,103]))
 
